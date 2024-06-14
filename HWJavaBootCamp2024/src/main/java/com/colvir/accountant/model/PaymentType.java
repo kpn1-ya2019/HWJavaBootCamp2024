3dc48ef671940e603b3a4f5ac322886059de02c4
kpn1-ya2019 <p.n.karpenko1@gmail.com>
2024-06-14 23:53:08 +0600

    HW1. - Добавил модель PaymentType - Проверка запуска http://localhost:8080/api/swagger-ui/index.html. Успешно

---
+++ HWJavaBootCamp2024/src/main/java/com/colvir/accountant/model/PaymentType.java
@@ -0,0 +1,14 @@
+package com.colvir.accountant.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentType {
+    private Long id;
+    private String name;
+
+}
